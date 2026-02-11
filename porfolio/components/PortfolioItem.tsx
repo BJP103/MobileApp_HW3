ae718de6fe03fffef3d1e662933b01c85d5c4db7
BJP103 <BJP103@pitt.edu>
2026-02-11 11:59:34 -0500

    commit

--- porfolio/components/PortfolioItem.tsx
+++ porfolio/components/PortfolioItem.tsx
@@ -1,11 +1,10 @@
+import data from './portfolio.json';
+
 export function Test(){
     return(
         <h1>This is a test</h1>
     )
 }
 export function PortfolioItem(){
-    /*return(
 
-
-    )*/
 }
